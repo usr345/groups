2411f02d6f0e50fd37354b130d73547f6fc07b2a
Ivanov Dmitriy <usr345@gmail.com>
2021-01-21 19:56:12 +0300

    Доказано a^(m + n) = a^m + a^n.

--- groups.v
+++ groups.v
@@ -274,11 +274,6 @@ Section Group_theorems.
   (* Proposition lemma1 : forall () zpow a (Z.pos_sub p p0) = (pow a p) * inv (pow a p0). *)
     (* Search Pos_to_nat. *)
   (* Proposition a_pow_m_n : forall (a: G) (n m: nat), (pow a n)*(pow a m) = pow a (n + m). *)
-  Lemma pow_a_n_plus_1_pos : forall (a: G) (n: positive), pow_z a (Z.succ (Zpos n)) = a * pow_z a (Zpos n).
-  Proof.
-    intros a n. simpl. rewrite (Pos2Nat.inj_add n 1). rewrite Nat.add_comm. rewrite <- a_pow_m_n. simpl. rewrite right_id. reflexivity.
-  Qed.
-
   Locate "-".
   Search ((1 + _)%positive).
   Search (Z.neg (_ + _)).
@@ -287,21 +282,31 @@ Section Group_theorems.
     intros a n. unfold Z.succ. unfold Z.add.
     destruct n.
     - simpl. reflexivity.
-    - apply pow_a_n_plus_1_pos.
+    - simpl. rewrite (Pos2Nat.inj_add p 1). rewrite Nat.add_comm. rewrite <- a_pow_m_n. simpl. rewrite right_id. reflexivity.
     - rewrite <- Pos2Z.add_pos_neg. induction p using Pos.peano_ind.
       + simpl. rewrite right_id. rewrite right_inv. reflexivity.
       + rewrite <- Pos.add_1_l. rewrite <- Pos2Z.add_neg_neg. rewrite Z.add_assoc. rewrite Z.add_opp_diag_r. rewrite Z.add_0_l. rewrite Pos2Z.add_neg_neg. unfold pow_z. rewrite Pos.add_comm. rewrite Pos2Nat.inj_add. simpl. rewrite <- a_pow_m_n. rewrite inv_prod. simpl. rewrite right_id. rewrite assoc. rewrite right_inv. rewrite left_id. reflexivity.
+  Qed.
+
+  Search (Z.pos (Pos.succ _)).
+  Search Z.neg.
+  Lemma pow_a_n_minus_1_Z : forall (a: G) (n: Z), (pow_z a (Z.pred n)) = inv(a) * pow_z a n.
+  Proof.
+    intros a n. unfold Z.pred. unfold Z.add.
+    destruct n.
+    - simpl. repeat rewrite right_id. reflexivity.
+    - rewrite <- Pos2Z.add_pos_neg. induction p using Pos.peano_ind.
+      + simpl. rewrite right_id. rewrite left_inv. reflexivity.
+      + rewrite Pplus_one_succ_l. rewrite Pos.add_1_l. rewrite Pos2Z.inj_succ. rewrite pow_a_n_plus_1_Z. rewrite assoc. rewrite left_inv. rewrite left_id. rewrite <- Z.add_1_r. rewrite <- Z.add_assoc. rewrite Pos2Z.add_pos_neg. simpl (Z.pos_sub 1 1). rewrite Z.add_0_r. reflexivity.
+    - simpl. rewrite (Pos2Nat.inj_add p 1). rewrite <- a_pow_m_n. rewrite inv_prod. simpl (a ** Pos.to_nat 1). rewrite right_id. reflexivity.
   Qed.
 
   Proposition a_pow_m_n_Z : forall (a: G) (n m: Z), (pow_z a n)*(pow_z a m) = pow_z a (n + m).
   Proof.
     intros. induction n using Z.peano_ind.
     - simpl. apply left_id.
-    - unfold Z.succ.
-    induction n as [| n' IH | n' IH].
-    -
-    - simpl. destruct m.
-      + simpl. apply right_id.
-      +
-
+    - rewrite pow_a_n_plus_1_Z. rewrite <- assoc. rewrite IHn. rewrite <- pow_a_n_plus_1_Z. rewrite Z.add_succ_l. reflexivity.
+    - rewrite pow_a_n_minus_1_Z. rewrite <- assoc. rewrite IHn.
+      rewrite Z.add_pred_l. rewrite pow_a_n_minus_1_Z. reflexivity.
+  Qed.
 End Group_theorems.
