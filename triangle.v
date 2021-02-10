7ac27845d49897273cc93d2ad01f5c40dc741600
Antonio Nikishaev <me@lelf.lu>
2021-02-10 21:04:04 +0300

    fix triangle

--- triangle.v
+++ triangle.v
@@ -1,6 +1,7 @@
 Add LoadPath ".".
 Require Import groups.
 Require Import groups_Z_n.
+Import GroupZ_3.
 
 Open Scope group_scope.
 Section polygons.
@@ -38,7 +39,7 @@ End polygons.
 Definition triangle := Build_coloured_graph Z_3 bool Nat.eqb.
 Definition rotate (z n: Z_3): Z_3 := Z3_op z n.
 
-  Compute (self_isometry triangle rotate).
+  (* Compute (self_isometry triangle rotate). *)
   Search "comp".
 
   Definition f (x: nat) := x + 1.
@@ -46,6 +47,7 @@ Definition rotate (z n: Z_3): Z_3 := Z3_op z n.
 
   Locate "∘".
   Compute (Basics.compose f g) 3.
+  Require Import Program.Basics.
   Open Scope program_scope.
   Theorem compose_assoc (A B C D: Type) (f: A -> B) (g: B -> C) (h: C -> D):
     h ∘ (g ∘ f) = (h ∘ g) ∘ f.
